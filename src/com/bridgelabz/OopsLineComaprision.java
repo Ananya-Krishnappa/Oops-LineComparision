73898ee448428dbaeacf0a54013eda8b98631b25
Ananya-Krishnappa <ananyak3395@gmail.com>
2021-06-06 13:13:51 +0530

    Added:Length of line

--- src/com/bridgelabz/OopsLineComaprision.java
+++ src/com/bridgelabz/OopsLineComaprision.java
@@ -1,7 +1,27 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class OopsLineComaprision {
+	static Scanner scanner = new Scanner(System.in);
+
 	public static void main(String[] args) {
-		System.out.println("Welcome line computation");
+		OopsLineComaprision length = new OopsLineComaprision();
+		System.out.println("Enter x1,x2,y1,y2 values");
+		calculateLength(length);
+	}
+
+	/**
+	 * This method is used to calculate the length of a line
+	 * 
+	 * @param length
+	 */
+	private static void calculateLength(OopsLineComaprision length) {
+		int x1 = scanner.nextInt();
+		int x2 = scanner.nextInt();
+		int y1 = scanner.nextInt();
+		int y2 = scanner.nextInt();
+		double length1 = Math.sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
+		System.out.println("length is " + length1);
 	}
 }
