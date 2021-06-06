927b099d3fe001ace05cfe5b1035b15dceaa1283
Ananya-Krishnappa <ananyak3395@gmail.com>
2021-06-06 13:19:21 +0530

    Added:Lines are equal

--- src/com/bridgelabz/OopsLineComaprision.java
+++ src/com/bridgelabz/OopsLineComaprision.java
@@ -7,8 +7,8 @@ public class OopsLineComaprision {
 
 	public static void main(String[] args) {
 		OopsLineComaprision length = new OopsLineComaprision();
-		System.out.println("Enter x1,x2,y1,y2 values");
-		calculateLength(length);
+		System.out.println("Enter x1,x2,y1,y2,a1,a2,b1,b2 values");
+		areLinesEqual(length);
 	}
 
 	/**
@@ -23,5 +23,26 @@ public class OopsLineComaprision {
 		int y2 = scanner.nextInt();
 		double length1 = Math.sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
 		System.out.println("length is " + length1);
+	}
+
+	/**
+	 * This method is checking if the two lines are equal
+	 * 
+	 * @param length
+	 */
+	private static void areLinesEqual(OopsLineComaprision length) {
+		int x1 = scanner.nextInt();
+		int x2 = scanner.nextInt();
+		int y1 = scanner.nextInt();
+		int y2 = scanner.nextInt();
+		int a1 = scanner.nextInt();
+		int a2 = scanner.nextInt();
+		int b1 = scanner.nextInt();
+		int b2 = scanner.nextInt();
+		Double length1 = Math.sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
+		Double length2 = Math.sqrt((a2 - a1) ^ 2 + (b2 - b1) ^ 2);
+		String equalMessage = (length.equals(length1) == true) ? "equal" : "not equal";
+		System.out.println("Length of two lines is " + equalMessage);
+
 	}
 }
