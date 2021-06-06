4c4ea9e529e8af72a1a98cee983b48fb826fd122
Ananya-Krishnappa <ananyak3395@gmail.com>
2021-06-06 13:24:20 +0530

    Added:Lines are equal or less than or greater

--- src/com/bridgelabz/OopsLineComaprision.java
+++ src/com/bridgelabz/OopsLineComaprision.java
@@ -8,7 +8,7 @@ public class OopsLineComaprision {
 	public static void main(String[] args) {
 		OopsLineComaprision length = new OopsLineComaprision();
 		System.out.println("Enter x1,x2,y1,y2,a1,a2,b1,b2 values");
-		areLinesEqual(length);
+		toCompareLines(length);
 	}
 
 	/**
@@ -44,5 +44,34 @@ public class OopsLineComaprision {
 		String equalMessage = (length.equals(length1) == true) ? "equal" : "not equal";
 		System.out.println("Length of two lines is " + equalMessage);
 
+	}
+
+	/**
+	 * This method is comparing two lines if they are equal or less than or greater
+	 * than
+	 * 
+	 * @param length
+	 */
+	private static void toCompareLines(OopsLineComaprision length) {
+		int x1 = scanner.nextInt();
+		int x2 = scanner.nextInt();
+		int y1 = scanner.nextInt();
+		int y2 = scanner.nextInt();
+		int a1 = scanner.nextInt();
+		int a2 = scanner.nextInt();
+		int b1 = scanner.nextInt();
+		int b2 = scanner.nextInt();
+		Double length1 = Math.sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
+		Double length2 = Math.sqrt((a2 - a1) ^ 2 + (b2 - b1) ^ 2);
+		int compareResult = length1.compareTo(length2);
+		String compareMessage;
+		if (compareResult == 0) {
+			compareMessage = "Length of line1 and line2 is equal";
+		} else if (compareResult < 0) {
+			compareMessage = "Length of line1 is less than line2";
+		} else {
+			compareMessage = "Length of line1 is greater than line2";
+		}
+		System.out.println(compareMessage);
 	}
 }
